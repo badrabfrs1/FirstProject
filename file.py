4d4207bbaa4051e6d4d1943b81d24bc611117460
badrabfrs1 <badr.ab2002@gmail.com>
2023-02-18 11:26:05 +0100

    Emphasise the name

---
+++ file.py
@@ -0,0 +1,76 @@
+"""
+character_name="Badr"
+character_age="20"
+print("Hello everyone my name is " + character_name + ",")
+print("Last year I turned " + character_age + "," )
+
+print("Giraffe \n Academy")
+
+phrase= "Hello World"
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("W"))
+print(phrase.replace("Hello" ,"Welcome"))
+
+
+
+print(2)
+my_num=5
+print(my_num)
+print(str(my_num) + " my favorite number")
+
+
+
+my_num1=-2
+print(abs(my_num1))
+
+print(pow(3,2))
+print(max(3,6))
+print(min(2,1))
+print(round(3.5))
+
+from math import *
+
+my_num2=-7
+print(floor(3.7))
+print(ceil(4.9))
+print(sqrt(36))
+
+
+name = input("Enter your name: ")
+agee = input("Enter your age: ")
+print("Hello " + name + "!")
+print("Your age is " + agee)
+"""
+
+"""
+#Calculator
+number1 = input("Enter the first number:")
+number2 = input("Enter the second number:")
+result= float(number1) + float(number2)
+print(result)
+
+
+#Mad libs
+color = input("Enter a color ")
+plural_noun = input("Enter a plural noun  ")
+celebrity = input("Enter a celebrity ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+"""
+
+
+#Lists
+friends= ["Badr" , "Haitem" , "Aymen" , "Salim"]
+lucky_numbers=[ 3 , 2 , 4 ,5 ]
+friends.append(lucky_numbers)
+print(friends)
+
+
+
+
+
+
